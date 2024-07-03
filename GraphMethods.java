ad4b50193d71b9610f1dd8a9fce2dd50c92f7490
nda122 <96876598+nda122@users.noreply.github.com>
2024-07-03 13:06:15 +0700

    Add files via upload

---
+++ GraphMethods.java
@@ -0,0 +1,31 @@
+package Termproject2;
+
+import java.util.*;
+
+public class GraphMethods {
+    static public ArrayList<Integer> BFS(Graph G, int start){
+        return null;
+    }
+    static public ArrayList<Integer> DFS(Graph G, int start){
+        return null;
+    }
+    static public boolean checkCycle(Graph G){
+        return false;
+    }
+    static public ArrayList<Integer> TopologicalSorting(Graph G){
+        return null;
+    }
+    static public int[] ShortestPath(Graph G, int start){
+        return null;
+    }
+    static public int MST(Graph G){
+        return 0;
+    }
+    static public boolean isBipartite(Graph G){
+        return false;
+    }
+    static public boolean isIsomorphic(Graph G1, Graph G2){
+        return false;
+    }
+    
+}
